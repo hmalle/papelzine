31ad97ec6685cd215475a98afb96c9a4239a7b4d
hmalle <hmalle90ak@gmail.com>
2018-04-07 20:16:44 -0700

    trying to get started here

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,7 +4,7 @@ import "./Form.css";
 
 const Form = props  =>
   <div className="container">
-    <h2>Form Form Form</h2>
+    <h2>Form to get all the input from the user and hit the backend routes</h2>
   </div>
 
 export default Form;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.Promise = Promise;
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/papelzine_db" ;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/papelzineDB" ;
 mongoose.connect(MONGODB_URI,function(err){
   if(err){ 
     console.log("Mongoose Connection Error: "+err);
